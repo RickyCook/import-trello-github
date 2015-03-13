51c8a4e446ae83d6e4f932a1e5a77e8c86001c3d
Ricky Cook <mail@thatpanda.com>
2015-03-13 18:54:19 +1100

    Add basic CLI skeleton

--- import-issues.py
+++ import-issues.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="Import Trello cards JSON into GitHub issues"
+)
+
+def main():
+    args = parser.parse_args()
+
+if __name__ == '__main__':
+    main()
